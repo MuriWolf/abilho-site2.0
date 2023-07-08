4d31f26ecbf4cbfd32a145d9343da86cc96019a4
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2023-07-08 13:18:27 -0300

    animation added and modifications

--- public/main.js
+++ public/main.js
@@ -1,22 +1,2 @@
-var element = document.getElementById('headerText');
-const typewriter = new Typewriter(element, {
-  autoStart: true,
-  loop: true,
-  delay: 75,
-});
+import { typewriter } from "./modules/animation.js";
 
-typewriter  
-  .pauseFor(1000)
-  .typeString('Links úteis')
-  .pauseFor(2000)
-  .deleteAll()
-  .typeString('Seu negócio mais profissional')
-  .pauseFor(2000)
-  .deleteAll()
-  .typeString('Criativos que vendem')
-  .pauseFor(2000)
-  .start();
-
-function newFunction() {
-  console.log("Anything")
-}
